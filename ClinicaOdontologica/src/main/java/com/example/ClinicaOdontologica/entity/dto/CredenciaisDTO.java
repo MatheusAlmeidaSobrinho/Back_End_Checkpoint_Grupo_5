65720e5412c5fb18cdb58363d82511dc0cd4911d
Tawan Silva <tawan.tls43@gmail.com>
2022-09-25 01:31:41 -0300

    credenciais dto

---
+++ ClinicaOdontologica/src/main/java/com/example/ClinicaOdontologica/entity/dto/CredenciaisDTO.java
@@ -0,0 +1,11 @@
+package com.example.ClinicaOdontologica.entity.dto;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class CredenciaisDTO {
+
+    private String login;
+    private String senha;
+}
