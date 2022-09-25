490df833b77c523ca65c2c669f9952afa3e5f59c
Tawan Silva <tawan.tls43@gmail.com>
2022-09-25 01:34:34 -0300

    endpoint e servico de login para dentista + PasswordException

---
+++ ClinicaOdontologica/src/main/java/com/example/ClinicaOdontologica/common/exception/PasswordInvalidException.java
@@ -0,0 +1,8 @@
+package com.example.ClinicaOdontologica.common.exception;
+
+public class PasswordInvalidException extends RuntimeException {
+
+    public PasswordInvalidException(String msg) {
+        super(msg);
+    }
+}
